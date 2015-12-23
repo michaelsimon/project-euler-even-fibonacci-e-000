af31f621b51f2e7a1817b5cc6e2e4df2f8d2d5fd
michaelsimon <michael.simon@gmail.com>
2015-12-22 23:22:11 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(num_count)
+  calc = 1
+  fib_values = Array[1]
+  fib_even = 0
+
+  until calc >= num_count
+    calc = calc + fib_values[fib_values.size-2]
+   if calc <= num_count
+    fib_values << calc
+    end
+
+  end
+
+  fib_values.each do |fib_value|
+    if fib_value % 2 == 0
+      fib_even = fib_even + fib_value   
+    end
+  end
+
+  return fib_even
+
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,32 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+attr_accessor :num_count
+
+  def initialize(num_count)
+    @num_count = num_count
+  end
+
+  def sum
+    calc = 1
+    fib_values = Array[1]
+    fib_even = 0
+
+    until calc >= @num_count
+      calc = calc + fib_values[fib_values.size-2]
+     if calc <= @num_count
+      fib_values << calc
+      end
+
+    end
+
+    fib_values.each do |fib_value|
+      if fib_value % 2 == 0
+        fib_even = fib_even + fib_value   
+      end
+    end
+
+    return fib_even
+
+  end
+end
